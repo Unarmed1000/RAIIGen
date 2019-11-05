36068b1718cc1d61fc505c16d5b06faafd6c0d5a
Rene Thrane <rene.thrane@manabattery.com>
2019-11-05 02:04:03 +0100

    Added Vulkan 1.1.121.0 Added Vulkan 1.1.126.0
    Upgraded project to VS2019

--- config/Templates/RapidVulkan/AddMethods/ShaderModule.hpp
+++ config/Templates/RapidVulkan/AddMethods/ShaderModule.hpp
@@ -2,6 +2,24 @@
     //! @brief Destroys any owned resources and then creates the requested one
     //! @note  Function: vkCreateShaderModule
     template <std::size_t TSize>
+    ShaderModule(const VkDevice device, const VkShaderModuleCreateFlags flags, const std::array<uint32_t, TSize>& code)
+      : ShaderModule()
+    {
+      Reset(device, flags, code.size(), code.data());
+    }
+
+    //! @brief Destroys any owned resources and then creates the requested one
+    //! @note  Function: vkCreateShaderModule
+    template <std::size_t TSize>
+    ShaderModule(const VkDevice device, const VkShaderModuleCreateFlags flags, const std::array<uint8_t, TSize>& code)
+      : ShaderModule()
+    {
+      Reset(device, flags, code.size(), reinterpret_cast<const uint32_t*>(code.data()));
+    }
+
+    //! @brief Destroys any owned resources and then creates the requested one
+    //! @note  Function: vkCreateShaderModule
+    template <std::size_t TSize>
     void Reset(const VkDevice device, const VkShaderModuleCreateFlags flags, const std::array<uint32_t, TSize>& code)
     {
       Reset(device, flags, code.size(), code.data());
@@ -18,6 +36,22 @@
 
 
 #if !defined(RAPIDVULKAN_DISABLE_UNROLLED_STRUCT_METHODS) && !defined(RAPIDVULKAN_DISABLE_VECTOR_METHODS)
+    //! @brief Destroys any owned resources and then creates the requested one
+    //! @note  Function: vkCreateShaderModule
+    ShaderModule(const VkDevice device, const VkShaderModuleCreateFlags flags, const std::vector<uint32_t>& code)
+      : ShaderModule()
+    {
+      Reset(device, flags, code.size(), code.data());
+    }
+
+    //! @brief Destroys any owned resources and then creates the requested one
+    //! @note  Function: vkCreateShaderModule
+    ShaderModule(const VkDevice device, const VkShaderModuleCreateFlags flags, const std::vector<uint8_t>& code)
+      : ShaderModule()
+    {
+      Reset(device, flags, code.size(), reinterpret_cast<const uint32_t*>(code.data()));
+    }
+
     //! @brief Destroys any owned resources and then creates the requested one
     //! @note  Function: vkCreateShaderModule
     void Reset(const VkDevice device, const VkShaderModuleCreateFlags flags, const std::vector<uint32_t>& code)
